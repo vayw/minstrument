8ac4860440bfcf78d1bc07cd35f1e76439efae18
vayw <vayw@botans.org>
2015-04-03 02:29:09 +0300

    added copy function

--- minstrument.py
+++ minstrument.py
@@ -7,6 +7,10 @@ import os
 import sys
 import argparse
 import configparser
+import shutil
+
+# supported formats
+SUPPORTED = ['flac', 'ogg']
 # don't know if it's necessary to use python libraries to work with flac
 # and ogg files. For beginning let's stop on commandline utilities
 # Will be nice to check it's availability
@@ -26,6 +30,7 @@ def get_util_present(_util):
     return is_available
 
 # function returns list of tags
+# it takes filename and config section for original file type, e.g. 'flac', 'ogg' etc.
 def get_track_data(filename, tagger):
     tagInUtility = tagger["tagUtility"]
     tagParams = tagger['params'].split()
@@ -68,7 +73,6 @@ def get_album_dir_hard(_tags_list, _filename, musicDir):
         if _our_string.startswith("ARTIST"):
             _artist = i[len("ARTIST="):]
             _tags_count+=1
-            print ("got it!")
         elif _our_string.startswith("ALBUM"):
             _album = i[len("ALBUM="):]
             _tags_count+=1
@@ -134,12 +138,15 @@ def get_album_dir_hard(_tags_list, _filename, musicDir):
         print("Unfortunately seems like there is no suitable folder in", musicDir)
         print("can you provide path?")
         _answer = input("yes/No: ")
-        if _answer is "yes" or _answer is "y":
+        if _answer == "yes" or _answer == "y":
             print("please type path to folder")
             _user_path = input("--> ")
-            return _user_path
+            if os.path.exists(_user_path):
+                return _user_path
+            else:
+                return -1
         else:
-            sys.exit("have no tracks to tag!")
+            return -1
     elif len(_folders) == 1:
         return _folder[0][0]
     else:
@@ -183,7 +190,7 @@ def get_album_dir(_file, inFileType, musicDir):
 def create_config(cfg):
     config = configparser.ConfigParser()
     _dir = input("enter path to your music library: ")
-    if _dir[-1] is not "/":
+    if _dir[-1] != "/":
         _dir = _dir + "/"
     if os.path.isdir(_dir) == False:
         print("Seems like this path is invalid, don't forget to create it.")
@@ -204,6 +211,50 @@ def create_config(cfg):
         print("oops! something went wrong with creating config file!")
         print(e)
 
+def copy(_args, _cfg):
+    _tag_list = 0
+    for i in os.listdir(_args.indir):
+        for filetype in SUPPORTED:
+           if i.endswith(filetype):
+               _file = i
+               _type = filetype
+               _tag_list = get_track_data(i, _cfg[filetype])
+               break
+        if _tag_list != 0: break
+
+    _dir = get_album_dir(_file, _type, _args.musicdir)
+    if _dir == -1:
+        _dir = get_album_dir_hard(_tag_list, _file, _args.musicdir)
+        if _dir == -1:
+            print("create directory?")
+            _answer = input("yes/No: ")
+            if _answer == "yes" or _answer == "y":
+                _tags_count = 0
+                for i in _tag_list:
+                    # as tags have no spec for writing let's capitalize our string
+                    _our_string = i.upper()
+                    if _our_string.startswith("ARTIST"):
+                        _artist = i[len("ARTIST="):]
+                        _tags_count+=1
+                    elif _our_string.startswith("ALBUM"):
+                        _album = i[len("ALBUM="):]
+                        _tags_count+=1
+                    elif _our_string.startswith("DATE"):
+                        _release_date = i[len("DATE="):]
+                        _tags_count+=1
+                    # now check tags_count
+                    if _tags_count == 3:
+                        break
+                _dir = _artist + "/" + _release_date + " - " + _album
+                print(_args.musicdir + "/" + _dir)
+                os.makedirs(_args.musicdir + "/" + _dir)
+                for i in os.listdir(_args.indir):
+                    if i.endswith("." + _type):
+                        shutil.copy(i, _args.musicdir + "/" + _dir)
+        else:
+            print("nothing to do")
+    sys.exit()
+
 def main():
     # now we work with commandline arguments
     parser = argparse.ArgumentParser()
@@ -211,6 +262,7 @@ def main():
     parser.add_argument("-o", "--outdir", help="directory, containing untagged files")
     parser.add_argument("-m", "--musicdir", help="directory with music collection")
     parser.add_argument("--outcodec", help="output codec")
+    parser.add_argument("--copy", action='store_true', help="create directory in library and copy tracks")
     args = parser.parse_args()
 
     # let's check config and create it if we don't have it
@@ -221,23 +273,27 @@ def main():
     else:
         _config = create_config(_cfg)
 
-    # now check available utils
-    available_utils = {}
-    for key in _config:
-        try:
-            available_utils[key] = get_util_present(_config[key]['tagUtility'])
-        except:
-            pass
-    if len(available_utils) < 2:
-        sys.exit("not enough utils available!")
-
-    tag_collection = {}
-    files_list = []
-
     if args.indir is None:
         args.indir = "./"
     if args.musicdir is None:
         args.musicdir = _config['general']['musicDir']
+
+    if args.copy is False:
+        # now check available utils
+        available_utils = {}
+        for key in _config:
+            try:
+                available_utils[key] = get_util_present(_config[key]['tagUtility'])
+            except:
+                pass
+        if len(available_utils) < 2:
+            sys.exit("not enough utils available!")
+    else:
+        copy(args, _config)
+
+    tag_collection = {}
+    files_list = []
+
     _inFileType = _config['general']['inFileType']
     _outFileType = _config['general']['outFileType']
 
@@ -258,6 +314,8 @@ def main():
     _folder = get_album_dir(files_list[-1], _inFileType, args.musicdir)
     if _folder == -1:
         _folder = get_album_dir_hard(tag_collection[files_list[-1]], files_list[-1], args.musicdir)
+        if _folder == -1:
+            sys.exit("directory doesn't exist!")
     print("files found in", _folder, "...")
     if _config['general']['outfiletype'] == "ogg":
         for _name in files_list:
